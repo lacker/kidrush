6457650b8a31807ade3ffe1b90ae289202383489
Kevin Lacker <lacker@gmail.com>
2019-02-08 10:23:31 -0800

    set up ids

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+let SIZE = 6;
+
+// Let the ids be a bunch of possible colors
+let PARTS = ["00", "33", "66", "99", "CC", "FF"];
+let IDS = [];
+for (let x of PARTS) {
+  for (let y of PARTS) {
+    for (let z of PARTS) {
+      IDS.push("#" + x + y + z);
+    }
+  }
+}
+
+function newGrid() {}
+
 export default function render() {
+  let [grid, setGrid] = useState(null);
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
