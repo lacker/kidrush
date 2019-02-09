9e1d275c605ecbe1b05259e96492ac34f86bd51c
Kevin Lacker <lacker@gmail.com>
2019-02-09 09:39:56 -0800

    Updates

---
+++ precache-manifest.12cd7f59f96b2a47f200439d0ed96419.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1be30df6ed3a693d33cc",
+    "url": "/kidrush/static/js/runtime~main.1be30df6.js"
+  },
+  {
+    "revision": "ae940431dd36c8ecaf13",
+    "url": "/kidrush/static/js/main.ae940431.chunk.js"
+  },
+  {
+    "revision": "7c60dad4794db4b90113",
+    "url": "/kidrush/static/js/1.7c60dad4.chunk.js"
+  },
+  {
+    "revision": "ae940431dd36c8ecaf13",
+    "url": "/kidrush/static/css/main.f8ee16f9.chunk.css"
+  },
+  {
+    "revision": "3d4aca0c2ca2b414477bef1e8873e4d0",
+    "url": "/kidrush/index.html"
+  }
+];
